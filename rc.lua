91d38580c5113567b8f62e6a7764cc22105f413d
minechan <cdetective2@gmail.com>
2023-11-15 16:10:50 +0900

    デスクトップエントリ

--- rc.lua
+++ rc.lua
@@ -2,7 +2,23 @@
 -- Awesomeの設定 --
 -------------------
 
-options = {}
+-- デバッグ用
+function print_table(t, indent)
+    indent = indent or ""
+    for key, value in pairs(t) do
+        if type(value) == "table" then
+            print(indent .. key .. ":")
+            print_table(value, indent .. "    ")
+        else
+            print(indent .. key .. ": " .. tostring(value))
+        end
+    end
+end
+
+options = {
+    -- 固定されたアプリケーション
+    pinned_apps = { "firefox", "kitty" }
+}
 
 -- アプリケーション
 require("applications")
